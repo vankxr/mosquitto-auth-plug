06d722d42aedd4437242b31c206a8ab0573b95c9
João Silva <jgc3silva@gmail.com>
2019-05-03 11:20:06 +0100

    Support MOSQ_ACL_SUBSCRIBE

--- auth-plug.c
+++ auth-plug.c
@@ -652,11 +652,11 @@ int mosquitto_auth_acl_check(void *userdata, const char *clientid, const char *u
 		return MOSQ_DENY_ACL;
 	}
 
-	_log(LOG_DEBUG, "mosquitto_auth_acl_check(..., %s, %s, %s, %s)",
+	_log(LOG_DEBUG, "mosquitto_auth_acl_check(..., %s, %s, %s, %d)",
 		clientid ? clientid : "NULL",
 		username ? username : "NULL",
 		topic ? topic : "NULL",
-		access == MOSQ_ACL_READ ? "MOSQ_ACL_READ" : "MOSQ_ACL_WRITE" );
+		access );
 
 
 	granted = acl_cache_q(clientid, username, topic, access, userdata);
